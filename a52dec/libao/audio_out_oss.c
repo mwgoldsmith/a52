6bd2e71853b4cbd5642b59a65588f67527a60dba
walken <walken@bb9bad2c-ca13-0410-88be-a4d44db82b4e>
2001-09-30 12:14:17 +0000

    bad freebsd forgot to define AFMT_S16_NE

--- a52dec/libao/audio_out_oss.c
+++ a52dec/libao/audio_out_oss.c
@@ -36,6 +36,14 @@
 #include <soundcard.h>
 #elif defined(__FreeBSD__)
 #include <machine/soundcard.h>
+#ifndef AFMT_S16_NE
+#include <machine/endian.h>
+#if BYTE_ORDER == LITTLE_ENDIAN
+#define AFMT_S16_NE AFMT_S16_LE
+#else
+#define AFMT_S16_NE AFMT_S16_BE
+#endif
+#endif
 #else
 #include <sys/soundcard.h>
 #endif
