d2596db39ea5d4c139e563a69d423ddb8ec5a086
walken <walken@bb9bad2c-ca13-0410-88be-a4d44db82b4e>
2002-02-16 09:58:51 +0000

    added aif file output - thanks to Shoji Tokunaga for contributing this.

--- a52dec/libao/audio_out.c
+++ a52dec/libao/audio_out.c
@@ -37,6 +37,8 @@ extern ao_open_t ao_solaris_open;
 extern ao_open_t ao_solarisdolby_open;
 extern ao_open_t ao_wav_open;
 extern ao_open_t ao_wavdolby_open;
+extern ao_open_t ao_aif_open;
+extern ao_open_t ao_aifdolby_open;
 extern ao_open_t ao_null_open;
 extern ao_open_t ao_null4_open;
 extern ao_open_t ao_null6_open;
@@ -55,6 +57,8 @@ static ao_driver_t audio_out_drivers[] = {
 #endif
     {"wav", ao_wav_open},
     {"wavdolby", ao_wavdolby_open},
+    {"aif", ao_wav_open},
+    {"aifdolby", ao_wavdolby_open},
     {"null", ao_null_open},
     {"null4", ao_null4_open},
     {"null6", ao_null6_open},
