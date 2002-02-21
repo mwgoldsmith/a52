580e9afc17c4ee9b6a5706efa2ad64b80b65f330
walken <walken@bb9bad2c-ca13-0410-88be-a4d44db82b4e>
2002-02-21 08:10:34 +0000

    99 bottles of beer, 1 down, 98 to go.

--- a52dec/vc++/config.h
+++ a52dec/vc++/config.h
@@ -79,7 +79,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.7.3"
+#define VERSION "0.7.4-cvs"
 
 /* Define if your processor stores words with the most significant byte first
    (like Motorola and SPARC, unlike Intel and VAX). */
